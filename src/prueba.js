f356fa50d3595ac9ef9052e92e5ef8978ccf836b
Maximiliano <maximartins45@gmail.com>
2022-06-27 23:46:18 -0300

    Update prueba.js

---
+++ src/prueba.js
@@ -0,0 +1,15 @@
+// import Footer from "./components/Footer";
+// import Home from "./components/Home";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <div className="container bg-light">
+//         <Home />
+//       <Footer/>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
